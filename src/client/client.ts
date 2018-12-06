66594379f1cc363f25711864ea240bb310d9622f
Bo Huang <bhuang1228@gmail.com>
2018-12-05 23:48:27 -0500

    typescript for func

---
+++ src/client/client.ts
@@ -0,0 +1,7 @@
+import { log } from 'lib';
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+log(sum(1, 2));
---
+++ src/server/server.ts
@@ -0,0 +1,7 @@
+import { log } from 'lib';
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+log(subtract(2, 1));
