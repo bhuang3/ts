47b2c7d3fbe41667ee939214f2a503bd89358484
Bo Huang <bo.huang@macbook-pro-22.jet.local>
2019-02-26 10:41:42 -0500

    more exercises

---
+++ src/typings/modules.ts
@@ -0,0 +1,4 @@
+// In Typescript, just as in ECMAScript 2015, any file containing a top-level import or export is considered a module
+//
+// A file without any top-level import or export declarations is treated as a script whose contents
+// are available in the gloabl scope
